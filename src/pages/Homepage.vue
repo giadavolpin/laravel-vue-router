9ea6b231e80db8aee06b18978046726f077f33b4
giadavolpin <volpingiada@gmail.com>
2023-01-18 16:44:44 +0100

    create cartelle varie

---
+++ src/pages/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        sono home page
+    </div>
+</template>
+
+<script>
+    export default {
+        name: HomePage
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
