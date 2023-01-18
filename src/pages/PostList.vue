e4c7d0e9c83dd7cb1b7dc3692d2014ed092c3244
giadavolpin <volpingiada@gmail.com>
2023-01-18 18:35:43 +0100

    pagine varie

---
+++ src/pages/PostList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        PostList
+    </div>
+</template>
+
+<script>
+    export default {
+        name: PostList
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Post single
+    </div>
+</template>
+
+<script>
+    export default {
+        name: SinglePost
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
